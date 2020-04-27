671aef447e5c3011dbe9126ddba327fc0729dba3
Brandon Davis <neusfear@gmail.com>
2020-04-26 19:09:09 -0500

    do some update and remove action

--- scripts/wiki.js
+++ scripts/wiki.js
@@ -15,79 +15,71 @@ function getXMLType() {
     }
 }
 
-function getMarkdownFrom(theUrl) {
-    xmlhttp = getXMLType();
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-            console.log(xmlhttp.responseText);
+function getMarkdown(data) {
+    var text = data.split("\n");
 
-            var text = xmlhttp.responseText.split("\n");
+    var parent = document.createElement("div");
+    parent.setAttribute("class", "container");
 
-            var parent = document.createElement("div");
-            parent.setAttribute("class", "container");
+    var sections = new Array();
+    var texts = new Array();
 
-            var sections = new Array();
-            var texts = new Array();
+    for (let i = 0; i < text.length; i++) {
+        console.log(text[i]);
+        if (text[i].startsWith("#")) {
+            //start a new section
+            var section = document.createElement("div");
+            section.setAttribute("class", "message is-dark");
+            
+            var title = document.createElement("div");
+            title.setAttribute("class", "message-header");
+            title.innerText = text[i].substring(1);
 
-            for (let i = 0; i < text.length; i++) {
-                if (text[i].startsWith("#")) {
-                    //start a new section
-                    var section = document.createElement("div");
-                    section.setAttribute("class", "message is-dark");
-                    
-                    var title = document.createElement("div");
-                    title.setAttribute("class", "message-header");
-                    title.innerText = text[i].substring(1);
+            section.appendChild(title);
+            sections.push(section);
 
-                    section.appendChild(title);
-                    sections.push(section);
-
-                    //starts a new array for subsequent texts under this section
-                    texts.push(new Array());
-                } else {
-                    //add text to current section
-                    texts[texts.length - 1].push(text[i]);
-                }
+            //starts a new array for subsequent texts under this section
+            texts.push(new Array());
+        } else {
+            //add text to current section ignoring empty lines
+            if (text.length != 0) {
+                texts[texts.length - 1].push(text[i]);
             }
-
-            for (let j = 0; j < sections.length; j++) {
-    
-                var body = document.createElement("div");
-                body.setAttribute("class", "message-body");
-
-                for (let k = 0; k < texts.length; k++) {
-                    body.innerText = body.innerText + "\n" + texts[j][k];
-                }
-    
-                sections[j].appendChild(body);
-                parent.appendChild(sections[j]);
-            }
-
-            updateHTML("wikicontent", parent);
         }
     }
-    xmlhttp.open("GET", theUrl, false );
-    xmlhttp.send();    
+
+    for (let j = 0; j < sections.length; j++) {
+
+        var body = document.createElement("div");
+        body.setAttribute("class", "message-body");
+
+        for (let k = 0; k < texts[j].length; k++) {
+
+            var textLine = document.createElement("p");
+            textLine.innerHTML = texts[j][k];
+
+            body.appendChild(textLine);
+        }
+
+        sections[j].appendChild(body);
+        parent.appendChild(sections[j]);
+    }
+
+    updateHTML("wikicontent", parent);
 }
 
 function readTextFile() {
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", "./wiki/wikipages/index.md", false);
-    rawFile.onreadystatechange = function () {
-        if(rawFile.readyState === 4) {
-            if(rawFile.status === 200 || rawFile.status == 0) {
-                var allText = rawFile.responseText;
-                alert(allText);
-            }
-        }
-    }
-    rawFile.send(null);
+    fetch("https://neusfear.github.io/fabric-wiki/wikipages/index.md")
+    .then(response => response.text())
+    .then((data) => {
+        getMarkdown(data);
+    })
 }
 
 function getFileStructureFromRepo() {
     xmlhttp = getXMLType();
-    xmlhttp.onreadystatechange=function() {
-        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 
             var jsonResult = JSON.parse(xmlhttp.responseText)
             var pages = new Array();
